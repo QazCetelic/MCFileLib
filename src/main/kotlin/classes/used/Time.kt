fcf848d4bc57e494ef8ddab88caa2eefcefb0527
Qaz <qaz.cetelic@gmail.com>
2020-12-24 08:59:48 +0100

    Restructured Time.kt

--- src/main/kotlin/classes/used/Time.kt
+++ src/main/kotlin/classes/used/Time.kt
@@ -1,10 +1,12 @@
 package classes.used
 
-data class Time(val year: Int,
-                val month: Int,
-                val day: Int,
-                val hour: Byte,
-                val minute: Byte,
-                val second: Byte) {
+data class Time (
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val hour: Byte,
+    val minute: Byte,
+    val second: Byte
+) {
     override fun toString() = "$year-$month-${day}_$hour.$minute.$second"
 }

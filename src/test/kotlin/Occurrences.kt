a505156390ff520fb6916feea4ab7f3dfcf60ed3
Qaz <qaz.cetelic@gmail.com>
2021-01-28 20:44:02 +0100

    Add Occurrences.kt

---
+++ src/test/kotlin/Occurrences.kt
@@ -0,0 +1,23 @@
+package testing
+
+import mcfilelib.util.Launchers
+
+fun ocurrences() {
+    val occurrences = mutableMapOf<String, Int>()
+
+    val launchers = Launchers.getAllInstalled()
+    launchers.forEach { launcher ->
+        launcher.instances.forEach { instance ->
+            instance.allMods.forEach { mod ->
+                mod.authors.forEach { author ->
+                    if (occurrences[author] != null) occurrences[author] = occurrences[author]!! + 1
+                    else occurrences[author] = 1
+                }
+            }
+        }
+    }
+
+    occurrences.forEach {
+        println(it.key + ": " + it.value)
+    }
+}
